a309745398c5a98808a58c469184de12f6c18c08
Andrea Villa <andrevillaciccio@gmail.com>
2021-12-01 12:33:46 +0100

    day 1 parts 1&2 completed

---
+++ 01.py
@@ -0,0 +1,14 @@
+with open('inputs/01.txt', 'r') as f:
+    depths = [int(depth.strip()) for depth in f.readlines()]
+    inc = 0
+    inc_roll = 0
+    for i, depth in enumerate(depths):
+        if i > 0 and depth > depths[i-1]:
+            inc += 1
+        if i > 2:
+            cur_roll = depth + depths[i-1] + depths[i-2]
+            prev_roll = depths[i-1] + depths[i-2] + depths[i-3]
+            if cur_roll > prev_roll:
+                inc_roll += 1
+    print(f'Part one solution: {inc}')
+    print(f'Part two solution: {inc_roll}')
