4965d471245440d1b42aa6244c88a3451a50b2b7
Andrea Villa <andrevillaciccio@gmail.com>
2021-12-02 01:19:17 +0100

    added jupyter notebook for day 1

--- 01.py
+++ 01.py
@@ -10,5 +10,5 @@ with open('inputs/01.txt', 'r') as f:
             prev_roll = depths[i-1] + depths[i-2] + depths[i-3]
             if cur_roll > prev_roll:
                 inc_roll += 1
-    print(f'Part one solution: {inc}')
-    print(f'Part two solution: {inc_roll}')
+    print(f'Part 1 solution: {inc}')
+    print(f'Part 2 solution: {inc_roll}')
